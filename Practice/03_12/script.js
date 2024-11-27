6533fbfa50a731e41f621fc80420943bf8d480cd
Jerome H <ralph.jerome.hernandez@outlook.com>
2024-11-27 23:37:38 +1100

    practice: Making classes and objects

--- Practice/03_12/script.js
+++ Practice/03_12/script.js
@@ -6,3 +6,27 @@
  * - Create several objects using the class.
  * - Test the objecs by calling their properties and using their methods in the console.
  */
+
+class Laptop {
+  constructor(name, year, os, screenSize, cpu) {
+    this.name = name;
+    this.year = year;
+    this.os = os;
+    this.screenSize = screenSize;
+    this.cpu = cpu;
+    this.power = false;
+  }
+
+  togglePower() {
+    this.power = !this.power;
+  }
+}
+
+const macbook = new Laptop("Macbook Pro", 2019, "MacOS", 13, {
+  name: "Intel Core i5",
+  cores: 4,
+  baseClock: 1.4,
+});
+
+macbook.togglePower();
+console.log(macbook);
