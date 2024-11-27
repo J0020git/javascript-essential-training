f390f2e66c63674032e4d3cd172ff8ffeed6c5c8
Jerome H <ralph.jerome.hernandez@outlook.com>
2024-11-27 23:18:32 +1100

    practice: building objects

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,17 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const macbook = {
+  name: "MacBook Pro",
+  year: 2019,
+  osName: "Sonoma",
+  screenSize: 13,
+  cpu: {
+    name: "Intel Core i5",
+    cores: 4,
+    baseClock: 1.4,
+  },
+};
+
+console.log(macbook);
