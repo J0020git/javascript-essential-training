049e5a4ead29c5054b0714fb77352b68f0532e59
Jerome H <ralph.jerome.hernandez@outlook.com>
2024-12-01 14:35:37 +1100

    practice: Modify classes, attributes, styles

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,18 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+const secondBackpack = document.querySelector("#pack02");
+
+secondBackpack.classList.add("class1");
+secondBackpack.classList.add("class2");
+secondBackpack.classList.remove("class2");
+
+console.log(secondBackpack.attributes);
+console.log(secondBackpack.hasAttribute("data-id"));
+secondBackpack.setAttribute("data-id", 2);
+console.log(secondBackpack.getAttribute("data-id"));
+secondBackpack.setAttribute("data-id", 22);
+console.log(secondBackpack.getAttribute("data-id"));
+
+const secondBackpackTitle = secondBackpack.querySelector("h1.backpack__name");
+secondBackpackTitle.style.color = "red";
