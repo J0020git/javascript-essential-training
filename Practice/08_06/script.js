0b23087e448e427ff9e5a86aa21c5cd9689fb584
Jerome H <ralph.jerome.hernandez@outlook.com>
2024-12-02 13:16:23 +1100

    practice: Build a function

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,26 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+// Function declaration
+function changeTitle1(newTitle) {
+  document.querySelector("title").innerText = newTitle;
+}
+
+// Function expression
+const changeTitle2 = function (newTitle) {
+  document.querySelector("title").innerText = newTitle;
+};
+
+// Arrow function expression
+const changeTitle3 = (newTitle) =>
+  (document.querySelector("title").innerText = newTitle);
+
+changeTitle1("One: The number 1");
+changeTitle2("Two: The number 2");
+changeTitle3("Three: The number 3");
+
+// Immediately-invoked function expression
+((newTitle) => (document.querySelector("title").innerText = newTitle))(
+  "Four: The number 4"
+);
